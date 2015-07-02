cc37bbf2133d8d741086fcfca2c25cd1c533c954
Benjamin Roberts <ben.roberts@nesi.org.nz>
2015-07-02 16:16:48 +1200

    Begin Python implementtaion of makedoc1

---
+++ makedoc1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+# This is a work in progress, reimplementing makedoc1 in python.
+# Python may be easier for managing module blacklists and mappings than
+# shell script.
+
+import os
+
